61af394c6b23b4cb6afc4542fa5df9261d120b26
Rayner <robrayner96@hotmail.com>
2019-09-26 21:45:57 +0100

    Init and beginnings

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float Exp;
+	public float Gold;
+
+
+    // Use this for initialization
+    void Awake () {
+
+        Exp = 500;
+        Gold = 500;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TestButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestButtons : MonoBehaviour
+{
+
+    Player player;
+    public Text goldText;
+    public Text expText;
+
+    public void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        if (player != null)
+            Debug.Log("Found The Player");
+        UpdateUI();
+    }
+
+    public void KillMob ()
+    {
+		player.Gold += 100;
+		player.Exp += 100;
+        UpdateUI();
+    }
+
+private void UpdateUI ()
+    {
+        goldText.text = "Gold: " + player.Gold;
+        expText.text = "Exp: " + player.Exp;
+    }
+
+}
